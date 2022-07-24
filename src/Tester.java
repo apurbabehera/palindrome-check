b941bffbc5879fad971f464cfcb5eba8474d6e2d
Apurba Behera <apurbabehera@noreply.github.com>
2022-07-24 14:36:55 +0530

    Initial commit

---
+++ src/Tester.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Tester{
+    public static boolean checkPalindrome(String str){
+	    //Implement your code here and change the return value accordingly
+    	String revStr = "";
+    	char[] charStrArr = str.toCharArray();
+    	for(int i=charStrArr.length-1; i>=0; i--) {
+    		revStr = revStr + charStrArr[i]; 
+    	}
+    	if (revStr.equals(str)) {
+    		return true;
+    	}
+        return false;
+	}
+	
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter input string for palindrome check");
+		String str = sc.nextLine();
+		if(checkPalindrome(str))
+			System.out.println("The string is a palindrome!");
+		else
+			System.out.println("The string is not a palindrome!");
+		sc.close();
+	}
+}
